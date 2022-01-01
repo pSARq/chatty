ae7527b8e7a02d7d629b976b59a25a6d5b726a20
Santiago Almendra <santiagoalmendrarojas19@gmail.com>
2022-01-01 14:47:07 -0500

    Se terminó el tutorial

---
+++ src/pages/Login.js
@@ -0,0 +1,94 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { signin, signInWithGoogle, signInWithGitHub } from "../helpers/auth";
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      email: "",
+      password: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault();
+    this.setState({ error: "" });
+    try {
+      await signin(this.state.email, this.state.password);
+    } catch (error) {
+      this.setState({ error: error.message });
+    }
+  }
+
+  async googleSignIn() {
+    try {
+      await signInWithGoogle();
+    } catch (error) {
+      
+      this.setState({ error: error.message });
+    }
+  }
+  async githubSignIn() {
+    try {
+      await signInWithGitHub();
+    } catch (error) {
+      this.setState({ error: error.message });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <form autoComplete="off" onSubmit={this.handleSubmit}>
+          <h1>
+            Login to
+            <Link to="/">Chatty</Link>
+          </h1>
+          <p>Fill in the form below to login to your account.</p>
+          <div>
+            <input
+              placeholder="Email"
+              name="email"
+              type="email"
+              onChange={this.handleChange}
+              value={this.state.email}
+            />
+          </div>
+          <div>
+            <input
+              placeholder="Password"
+              name="password"
+              onChange={this.handleChange}
+              value={this.state.password}
+              type="password"
+            />
+          </div>
+          <div>
+            {this.state.error ? <p>{this.state.error}</p> : null}
+            <button type="submit">Login</button>
+          </div>
+          <button className="boton-crear" onClick={this.googleSignIn} type="button">
+            Login with Google
+          </button>
+          <button className="boton-crear" type="button" onClick={this.githubSignIn}>
+            Login with GitHub
+          </button>
+          <hr />
+          <p>
+            Don't have an account? <Link to="/signup">Sign up</Link>
+          </p>
+        </form>
+      </div>
+    );
+  }
+}
