25e571880f37f25764ca3a5a04b6a48fa142214e
Santiago Almendra <santiagoalmendrarojas19@gmail.com>
2022-01-01 20:57:20 -0500

    Se empezaron las pruebas

---
+++ src/test/appTest.js
@@ -0,0 +1,15 @@
+import * as app from "../App";
+import Chat from "../pages/Chat";
+
+// "test": "react-scripts test",
+
+// test("private route", () =>{
+//     app.PrivateRoute(Chat, false, "/chat");
+//     expect(app.PrivateRoute).toHaveBeenCalledWith(Chat, false, "/chat");
+// })
+
+
+
+
+
+
